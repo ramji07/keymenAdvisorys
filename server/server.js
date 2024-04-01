6936fc33b8bbe3605ae8b5013273c15964a61181
RamjeeC <112837025+RamjeeC@users.noreply.github.com>
2024-04-01 14:26:55 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import Connection from './config/db.Connection.js'
+import Routes from './Routes/user.Routes.js'
+import cors from 'cors'
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cors());
+
+
+// Define routes
+app.use('/api', Routes)
+app.get('/',(req,res)=>{
+  res.status(200).send("hello World")
+})
+
+
+Connection()
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server is Running`)
+})
